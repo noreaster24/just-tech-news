4c600614d92cb5a5c221c3a76e942e095d017cd3
Joe Porrazzo <joepa2624@me.com>
2021-06-12 16:09:11 -0700

    final code review

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,6 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our db
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -14,4 +13,5 @@ if (process.env.JAWSDB_URL) {
     port: 3306
   });
 }
+
 module.exports = sequelize;
